62c0b3e9dc6ea91610915a42fe24fecd97d6489c
Nazar_Yaremiiy <nazar.yaremiiy@gmail.com>
2024-01-08 11:02:49 +0200

    Create update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,43 @@
+import User from '../models/user.model.js';
+import { errorHandler } from '../utils/error.js';
+import bcryptjs from 'bcryptjs';
+
 export const test = (req, res) => {
   res.json({
     message: 'Hello world!',
   });
+};
+
+// upload user
+
+export const updateUser = async (req, res, next) => {
+  if (req.user.id !== req.params.id) {
+    return next(errorHandler(401, 'You can update only your account!'));
+  }
+  try {
+    if (req.body.password) {
+      req.body.password = await bcryptjs.hashSync(req.body.password, 10);
+    }
+
+    const { username, email, password, profilePicture } = req.body;
+
+    const updateUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username,
+          email,
+          password,
+          profilePicture,
+        },
+      },
+      { new: true }
+    );
+
+    const { password: hashedPassword, ...rest } = updateUser._doc;
+
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
 };
