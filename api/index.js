71edfb173b88f999404b6062075dd0d6b19a2391
Nazar_Yaremiiy <nazar.yaremiiy@gmail.com>
2024-01-12 22:06:09 +0200

    Add pre production changes

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@ import cookieParser from 'cookie-parser';
 import userRoutes from './routes/user.route.js';
 import authRoutes from './routes/auth.route.js';
 
+import path from 'path';
+
 dotenv.config();
 
 mongoose
@@ -19,11 +21,18 @@ mongoose
 
 const app = express();
 
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
 app.use(express.json());
 app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log('Server listening on port 3000...');
+});
+
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 });
 
 // http://localhost:3000/api/user/
